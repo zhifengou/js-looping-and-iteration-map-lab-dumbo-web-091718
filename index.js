79cfaf7564668ba13e6e99624e8987874597e000
zhifeng ou <zhifengou@hotmail.com>
2019-04-04 21:43:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file.
+
+function lowerCaseDrivers(arr){
+  return arr.map(i=>i.toLowerCase());
+}
+
+function nameToAttributes(arr) {
+  return arr.map(i => {
+  return{firstName: i.split(" ")[0], lastName: i.split(" ")[1]}
+  })
+}
+
+function attributesToPhrase(arr) {
+  return arr.map(i => `${i.name} is from ${i.hometown}`)
+}
